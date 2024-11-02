f39fdc1022f3486153164367b922eb4dd06b7cf6
Aaryan Singh <aaryan.jsingh@gmail.com>
2024-11-01 19:38:57 -0600

    Cleaned PostFragment and changed to AddFragment

--- app/src/main/java/com/example/instagramclone/HomeActivity.kt
+++ app/src/main/java/com/example/instagramclone/HomeActivity.kt
@@ -23,7 +23,6 @@ class HomeActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment_activity_home)
 
-        // setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
